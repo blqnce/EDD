32a7eb76503b388d2e11e46540e0e1146d8522f4
Esteban Navas <198956144+blqnce@users.noreply.github.com>
2025-05-27 23:22:36 -0500

    Add files via upload

---
+++ EDD_Proyecto/Proyecto03/proyeccion.h
@@ -0,0 +1,17 @@
+/**********************************
+Fecha 5 de Marzo 2025
+Autor: Santiago Hernandez, Juan Esteban Bello, Esteban Navas
+Materia: Estructuras de Datos
+Tema: Primera entrega Proyecto
+***********************************/
+#ifndef PROYECCION_H
+#define PROYECCION_H
+
+#include "imagen.h" // Incluir la definición de la estructura imagen y volumen
+#include <algorithm> // Incluir la librería para usar funciones como min_element, max_element, etc.
+
+// Declaración de la función para generar una proyección 2D
+imagen proyeccion2D(volumen vol, string direccion, string criterio);
+
+#include"proyeccion.hxx"
+#endif // PROYECCION_H
